6da8a96344c8d39bddb79e18e33079a331065791
ClementFournier <Clement_F@outlook.com>
2017-03-25 16:47:57 -0400

    update creation fonction principales

--- RoboMain.ino
+++ RoboMain.ino
@@ -4,18 +4,25 @@ Servo servoCam;
 
 int motorRight1 = 3; // PIN 3
 int motorRight2 = 5; // PIN 5
-int enablePin = 5; // pin 1 on L293D IC
+
 int state;
 int flag=0;        //makes sure that the serial only prints once the state
+
+//setup bluetooth commands
+
+int commandRight = 1;
+int commandLeft = 2;
+int commandBackward = 3;
+int commandForward = 4;
 
 void setup() {
     // sets the pins as outputs:
     pinMode(motorRight1, OUTPUT);
     pinMode(motorRight2, OUTPUT);
-    pinMode(enablePin, OUTPUT);
+    //pinMode(enablePin, OUTPUT);
     
     // sets enablePin high so that motor can turn on:
-    digitalWrite(enablePin, HIGH);
+    //digitalWrite(enablePin, HIGH);
     
     // initialize serial communication at 9600 bits per second:
     Serial.begin(9600);
@@ -44,21 +51,55 @@ void loop() {
     
     // if the state is '1' the motor will turn right
     else if (state == '1') {
-        digitalWrite(motorRight1, LOW); // set pin 2 on L293D low
-        digitalWrite(motorRight2, HIGH); // set pin 7 on L293D high
-        if(flag == 0){
-          Serial.println("Motor: right");
-          flag=1;
-        }
+        right();
     }
     
     // if the state is '2' the motor will turn left
     else if (state == '2') {
-        digitalWrite(motorRight1, HIGH); // set pin 2 on L293D high
-        digitalWrite(motorRight2, LOW); // set pin 7 on L293D low
-        if(flag == 0){
-          Serial.println("Motor: left");
-          flag=1;
-        }
+        left();
+    }
+    
+    else if (state == '3') {
+        backward();
+    }
+    
+    else if (state == '4') {
+        forward();
     }
 }
+
+
+
+
+void left (){
+    digitalWrite(motorRight1, HIGH); // set pin 2 on L293D high
+    digitalWrite(motorRight2, LOW); // set pin 7 on L293D low
+    if(flag == 0){
+        Serial.println("Motor: left");
+        flag=1;
+    }
+}
+
+void right(){
+     digitalWrite(motorRight1, LOW); // set pin 2 on L293D low
+     digitalWrite(motorRight2, HIGH); // set pin 7 on L293D high
+     if(flag == 0){
+        Serial.println("Motor: right");
+        flag=1;
+     }
+}
+
+
+
+
+void forward (){
+
+}
+
+
+
+
+void backward () {
+
+}
+
